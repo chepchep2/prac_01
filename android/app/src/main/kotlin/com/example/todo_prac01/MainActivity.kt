21af2071c58fa596c91c6d5eed71cb165c7a4376
sangwoo <kajer102@gmail.com>
2025-05-20 22:39:29 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/todo_prac01/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.todo_prac01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/views/todo_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodoList extends StatelessWidget {
+  const TodoList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('todo 앱 시작')));
+  }
+}
