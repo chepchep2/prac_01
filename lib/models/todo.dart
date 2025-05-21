d6f33f86ec423f32a58b62881ff9377fefe105bd
sangwoo <kajer102@gmail.com>
2025-05-21 21:35:20 +0900

    Todo class에서 isCompleted는 완료/미완료에 따라 다르기 때문에 final X

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -2,7 +2,7 @@ class Todo {
   final int id;
   final String title;
   final String description; // title로만 할 경우 필요 없음
-  final bool isCompleted;
+  bool isCompleted;
 
   Todo({
     required this.id,
