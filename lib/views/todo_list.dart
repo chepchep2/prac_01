cd7b955bff82d6dbe0795641dc1a757e3c3d0792
sangwoo <kajer102@gmail.com>
2025-05-24 21:31:06 +0900

    main화면 상단에 textfield랑 +아이콘 추가, 아이콘은 추 후 수정 예정

--- lib/views/todo_list.dart
+++ lib/views/todo_list.dart
@@ -5,6 +5,32 @@ class TodoList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('todo 앱 시작')));
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "할일 추가",
+                        hintStyle: TextStyle(color: Colors.grey),
+                        enabledBorder: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  ElevatedButton(onPressed: () {}, child: Icon(Icons.add)),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
